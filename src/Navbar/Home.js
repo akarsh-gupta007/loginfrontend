fdf962396c626c519eff7317b805f65017ec943c
Akarshgu <akarshgupta705415@gmail.com>
2023-03-10 14:43:51 +0530

    okkkkk

--- src/Navbar/Home.js
+++ src/Navbar/Home.js
@@ -9,7 +9,7 @@ import { Container } from '@mui/system';
 
 const Home = (props) => {
 
-  const handlebtn=()=>{
+  const handlebtn = () => {
     props.setLoginUser({})
   }
   // console.log(setLoginUser.firstName)
@@ -20,27 +20,34 @@ const Home = (props) => {
       <CssBaseline />
       <Box
         sx={{
-
           marginTop: 15,
-          marginBottom: 45,
+          marginBottom: 26,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           borderRadius: "10px",
-          height: "150px",
-          boxShadow: "0px 0px 10px grey"
+          height: "300px",
+          boxShadow: "0px 0px 10px grey",
+          backgroundColor:"lightpink"
         }}
       >
 
-        <Typography component="h1" variant="h5">
-          Welcome {props.user.firstName} {props.user.lastName}
-        </Typography>
+        <Typography component="h1" variant="h5"
+        sx={{
+         textAlign:"center",
+         marginTop:"100px",
+         fontFamily:"sans-serif",
+         fontSize:"35px"
+        }}
+        >
+          Welcome {props.user.firstName} 
+          </Typography>
         <Button
-        onClick={handlebtn}
-        
+          onClick={handlebtn}
+
           fullWidth
           variant="contained"
-          sx={{ mt: 6, mb: 2 }}
+          sx={{ mt: 23, mb: 2,height:"120px" }}
         >
           Logout
         </Button>
